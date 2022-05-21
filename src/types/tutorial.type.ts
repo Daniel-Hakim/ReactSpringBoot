db92e67627fa1900af4f074263d6052ef551f971
hakim <danbelhak14@gmail.com>
2022-05-21 12:04:52 -0400

    react JS spring boot

---
+++ src/types/tutorial.type.ts
@@ -0,0 +1,11 @@
+export default interface ITutorialData {
+    id?: any | null,
+    title: string,
+    description: string,
+    published?: boolean,
+  }
+
+//   Now we need to define the data type for Tutorial. Create and export ITutorialData interface in types/tutorial.type.ts.
+//https://www.bezkoder.com/react-hooks-redux-crud/
+//https://www.bezkoder.com/react-redux-crud-example/
+//https://www.bezkoder.com/axios-request/
